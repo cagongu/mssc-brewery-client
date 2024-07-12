388720c3793d1cb8638c129da19cf666f5ed7886
cagongu <hophucthai9@gmail.com>
2024-07-13 02:00:19 +0700

    add http-put

---
+++ src/main/java/guru/springframework/msscbreweryclient/client/BreweryClient.java
@@ -0,0 +1,34 @@
+package guru.springframework.msscbreweryclient.client;
+
+import guru.springframework.msscbreweryclient.model.BeerDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.UUID;
+
+@Component
+public class BreweryClient {
+
+    public final String BEER_PATH_V1 = "/api/v1/beer";
+    private final String apihost;
+
+    private final RestTemplate restTemplate;
+
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder, @Value("${sfg.brewery.apihost}") String apihost) {
+        this.restTemplate = restTemplateBuilder.build();
+        this.apihost = apihost;
+    }
+
+    public BeerDto getBeerById(UUID uuid) {
+        return restTemplate.getForObject(apihost + BEER_PATH_V1 + "/" + uuid.toString(), BeerDto.class);
+    }
+
+    public URI saveNewBeer(BeerDto beerDto) {
+        return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDto);
+    }
+
+
+}

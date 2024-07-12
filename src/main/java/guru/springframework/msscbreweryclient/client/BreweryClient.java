b5e436eb693dda209e4f63f9675d04bf383d532a
cagongu <hophucthai9@gmail.com>
2024-07-13 02:11:49 +0700

    add method delete and test

--- src/main/java/guru/springframework/msscbreweryclient/client/BreweryClient.java
+++ src/main/java/guru/springframework/msscbreweryclient/client/BreweryClient.java
@@ -30,11 +30,11 @@ public class BreweryClient {
         return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDto);
     }
 
-
-
     public void updateBeer(UUID uuid, BeerDto beerDto){
         restTemplate.put(apihost + BEER_PATH_V1 + "/" + uuid.toString(), beerDto);
     }
 
-
+    public void deleteBeer(UUID uuid){
+        restTemplate.delete(apihost + BEER_PATH_V1 + "/" + uuid );
+    }
 }
